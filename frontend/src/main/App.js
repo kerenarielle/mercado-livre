6b9fe12e2874fc127800256e1eb31c9a8c2a9341
Keren Arielle <keren.ariellemr@gmail.com>
2019-05-13 19:53:16 -0300

    Nova estrutura de projeto

---
+++ frontend/src/main/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../logo.svg';
+import Header from './header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
---
+++ frontend/src/main/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <header>
+      <div class="container">
+        <h1>
+          <a>Logo mercado livre</a>
+        </h1>
+
+        <form>
+          <label>Nunca dejes de buscar</label>
+          <input type="text" />
+          <button></button>
+        </form>
+
+      </div>
+
+    </header>
+  )
+}
+
+export default Header;
